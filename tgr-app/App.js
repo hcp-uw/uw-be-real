7fa3374ac5be3924e43a86534837b5ad3c2125d1
Steven Tran <steven.tran077@gmail.com>
2023-01-17 15:33:11 -0800

    Added Camera module and set up navigation

--- tgr-app/App.js
+++ tgr-app/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CameraScreen from "./src/screens/CameraScreen";
+import PreviewScreen from "./src/screens/PreviewScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Camera">
+        <Stack.Screen name="Camera" component={CameraScreen} options={{unmountOnBlur: true}}/>
+        <Stack.Screen name="Preview" component={PreviewScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ tgr-app/src/camera/PreviewScreen.js
@@ -0,0 +1,66 @@
+import React from 'react'; 
+import { SafeAreaView, Image, Button, StyleSheet, Pressable, TextInput, KeyboardAvoidingView } from 'react-native';
+import { shareAsync } from 'expo-sharing';
+import { useState } from 'react';
+const PreviewScreen = ({route, navigation}) => {
+  const {photo, backPhoto} = route.params;
+  const [showFront, setShowFront] = useState(true);
+  const [caption, setCaption] = useState('');
+  let sharePic = () => {
+    shareAsync(photo.uri).then(() => {
+      setPhoto(undefined);
+    });
+  }
+  
+    
+    
+    return (
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView style={styles.preview}behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <Pressable style={styles.preview} onPress={() => setShowFront(!showFront)}>
+          <Image 
+            style={styles.preview} 
+            source={{ uri: "data:image/jpg;base64," 
+            +  (showFront ? photo.base64: backPhoto.base64) }}
+            
+          />
+        </Pressable></KeyboardAvoidingView>
+        <TextInput
+          style={styles.input}
+          text={caption}
+          onChangeText={(newValue) => {
+            setCaption(newValue);
+          }}
+        />
+        <Button title="Share" onPress={sharePic} />
+        <Button title="Discard" onPress={() => {
+          setPhoto(undefined);
+          setBackPhoto(undefined);
+
+        }} />
+      </SafeAreaView>
+    );
+
+    };
+
+const styles = StyleSheet.create({
+  preview: {
+    alignSelf: 'stretch',
+    flex: 1,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    borderRadius: 30
+  }, 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+  },
+  input: {
+    backgroundColor: 'black',
+    color: 'white',
+    margin: 5
+  }
+})
+export default PreviewScreen;
