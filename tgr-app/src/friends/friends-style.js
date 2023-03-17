6cf545996e228d86023315bcd1773358ecee608e
AntLe12 <le672869@gmail.com>
2023-03-16 22:34:20 -0700

    Formatting mostly done for friends and requests.

--- tgr-app/src/friends/friends-style.js
+++ tgr-app/src/friends/friends-style.js
@@ -11,7 +11,7 @@ export const styles = StyleSheet.create({
   },
   header: {
     top: 30,
-    flex: 0.06,
+    flex: 0.4,
     padding: 30
   },
   headerText: {
@@ -28,6 +28,55 @@ export const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     borderColor: 'white'
+  },
+  input: {
+    width: width * 0.9,
+    height: width*0.1,
+    backgroundColor: '#1A1A1A',
+    color: '#FFFFFF',
+    textAlign: 'left',
+    left: 23,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderRadius: 15, 
+    fontSize: 16,
+  },
+  myFriendsText: {
+    color: '#adadad',
+    paddingTop: 10,
+    paddingBottom: 10,
+    left: 23,
+    fontWeight: 'bold',
+  },
+  pendingFriendsText: {
+    color: '#adadad',
+    paddingTop: 10,
+    paddingBottom: 10,
+    left: 23,
+    fontWeight: 'bold',
+  },
+  friendsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    left: 23
+  },
+  friendsList: {
+    height: width * 0.45
+  },
+  requestsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    left: 23,
+  },
+  userText: {
+    color: '#959499',
+    top: 10,
+    left: 5,
+  },
+  friendProfPic: {
+    width: '10%',
+    height: width * 0.10,
+    borderRadius: 60,
   },
   navbar: {
     position: 'absolute',
@@ -48,24 +97,5 @@ export const styles = StyleSheet.create({
     position: 'absolute',
     left: (width * 0.76)-20,
     bottom: -830
-  }, 
-  profileInfo: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
   },
-  userContainer: {
-    flexDirection: 'row',
-  },
-  input: {
-    width: width * 0.9,
-    height: width*0.1,
-    backgroundColor: '#1A1A1A',
-    color: '#FFFFFF',
-    textAlign: 'left',
-    left: 23,
-    paddingHorizontal: 20,
-    borderWidth: 1,
-    borderRadius: 15, 
-    fontSize: 16,
-},
 });
--- tgr-app/src/friends/friends.js
+++ tgr-app/src/friends/friends.js
@@ -1,20 +1,23 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { FlatList, Text, Image, View, TouchableWithoutFeedback, TextInput, ScrollView } from 'react-native';
 import { Foundation, MaterialIcons, Ionicons } from '@expo/vector-icons';
 import { styles } from './friends-style';
 
 export default function Friends({navigation, route}) {
   const [profilePic, setProfilePic] = useState();
-    useMemo(() => {
+  const [friends, setFriends] = useState();
+  const [numFriends, setNumFriends] = useState(0);
+
+    useEffect(() => {
       const fetchProfile = fetch('https://haosenli.com/data/tgr_dummy_posts.json').then(res => res.json()).then(data => {
         setProfilePic(data[0].author_icon);
       });
 
       const fetchFriends = fetch('https://haosenli.com/data/tgr_dummy_friends.json').then(res => res.json()).then(data => {
-        // setProfilePic(data[0].author_icon);
+        setFriends(data.friends);
+        setNumFriends(Object.keys(data.friends).length);
       });
   }, []);
-
 
   return (
     <View style={styles.container}>
@@ -32,13 +35,67 @@ export default function Friends({navigation, route}) {
       </View>
 
       {/* Friend search bar */}
-      {/* <View> */}
-      <ScrollView keyboardShouldPersistTaps='handled'
-                  scrollEnabled={false}>
-          <TextInput style={styles.input} placeholder="Add or search friends"
-                    placeholderTextColor="#5A5A5A"></TextInput>
-      </ScrollView>
-      {/* </View> */}
+      <View style={styles.searchBar}>
+        <ScrollView keyboardShouldPersistTaps='handled'
+                    scrollEnabled={false}>
+            <TextInput style={styles.input} placeholder="Add or search friends"
+                      placeholderTextColor="#C0C0C0"></TextInput>
+        </ScrollView>
+      </View>
+
+      <Text style={styles.myFriendsText}>My Friends ({numFriends}) </Text>
+
+      {/* Friends list */}
+      <FlatList
+        style={styles.friendsList}
+        data={friends}
+        scrollEnabled={false}
+        renderItem={({ item }) =>                    
+          <View style={styles.friendsContainer}>
+            <TouchableWithoutFeedback  onPress={() => 
+              navigation.navigate('Profile')
+            }>
+              <Image
+                      style={styles.friendProfPic} 
+                      source={{uri: item.icon}}
+              />
+            </TouchableWithoutFeedback>
+
+            {/* Username and info on post */}
+            <View>
+            <Text style={styles.userText}>
+              {item.username}
+            </Text>
+            </View>
+          </View>}
+        ItemSeparatorComponent={() => <View style={{height: 20}} />}
+      />
+
+      <Text style={styles.pendingFriendsText}>Pending Requests ({numFriends}) </Text>
+
+      <FlatList
+        data={friends}
+        scrollEnabled={false}
+        renderItem={({ item }) =>                    
+          <View style={styles.requestsContainer}>
+            <TouchableWithoutFeedback  onPress={() => 
+              navigation.navigate('Profile')
+            }>
+              <Image
+                      style={styles.friendProfPic} 
+                      source={{uri: item.icon}}
+              />
+            </TouchableWithoutFeedback>
+
+            {/* Username and info on post */}
+            <View>
+            <Text style={styles.userText}>
+              {item.username}
+            </Text>
+            </View>
+          </View>}
+        ItemSeparatorComponent={() => <View style={{height: 20}} />}
+      />
 
       {/* Navbar */}
       <View style={styles.navbar}>
