1efbb32ebcbdbbc468b0ea08a07de5a7912e7bb9
Haosen Li <haosenli@Haosens-MacBook-Pro-15.local>
2023-05-05 03:57:56 -0700

    schema formatting

--- src/model/schemas/user_schema.py
+++ src/model/schemas/user_schema.py
@@ -105,8 +105,23 @@ USER_API_SCHEMA = {
 }
 
 CREATE_USER_SCHEMA = {
-    "netid": {"required": True, "type": NETID},
-    "email": {"required": True, "type": "string", "regex": UW_EMAIL_REGEX},
-    "username": {"required": True, "type": "string", "regex": USERNAME_REGEX},
-    "fullname": {"required": True, "type": "string", "regex": FULLNAME_REGEX},
+    "netid": {
+        "required": True,
+        "type": NETID,
+    },
+    "email": {
+        "required": True,
+        "type": "string",
+        "regex": UW_EMAIL_REGEX,
+    },
+    "username": {
+        "required": True,
+        "type": "string",
+        "regex": USERNAME_REGEX,
+    },
+    "fullname": {
+        "required": True,
+        "type": "string",
+        "regex": FULLNAME_REGEX,
+    },
 }
